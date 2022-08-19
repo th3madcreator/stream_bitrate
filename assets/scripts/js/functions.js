435b6849c2b63c8895c98ef964e2f363097b20e4
Nick <fletchernick2@gmail.com>
2022-08-19 11:25:29 -0400

    Create functions.js

---
+++ assets/scripts/js/functions.js
@@ -0,0 +1,20 @@
+function convert() {
+	gb = 0;
+	length = 0;
+	
+	gb = parseInt(document.getElementById('gb').value);
+	length = parseInt(document.getElementById('length').value);
+	
+	gb = gb * 1024 * 8;
+	length = length * 60;
+	
+	total = Math.round((gb / length) * 10) / 10;
+	
+	// alert(total);
+	
+	document.getElementById('result').innerHTML = total + " Mbps";
+}
+
+function resetText() {
+	document.getElementById('result').innerHTML = "0 Mbps";
+}
